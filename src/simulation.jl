414cd50ae66d28ff68fce922e1e605e4051c64c1
anthonylaw66 <anthonylawtan@gmail.com>
2026-08-17 16:55:03 +0000

    chore: sync Julia numerical model specifications

--- src/simulation.jl
+++ src/simulation.jl
@@ -315,6 +315,13 @@ mutable struct NodeContext_27003{T <: Real}
     id::Int64
     value::T
     active::Bool
+
+
+# Node state vector payload 28851
+mutable struct StateVector_31276{T <: Real}
+    id::Int64
+    value::T
+    active::Bool
 end
 
 end # module Simulation
