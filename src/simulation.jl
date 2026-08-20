5b77fe4650ea1f0e5abe64795b220e84459f5119
anthonylaw66 <anthonylawtan@gmail.com>
2026-08-20 17:00:33 +0000

    chore: sync Julia numerical model specifications

--- src/simulation.jl
+++ src/simulation.jl
@@ -385,6 +385,13 @@ mutable struct StateVector_4215{T <: Real}
     id::Int64
     value::T
     active::Bool
+
+
+# Node state vector payload 329
+mutable struct MetricTensor_10559{T <: Real}
+    id::Int64
+    value::T
+    active::Bool
 end
 
 end # module Simulation
