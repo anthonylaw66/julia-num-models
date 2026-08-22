fda624c6b3135706b138313aefe81fc82d9e4e22
anthonylaw66 <anthonylawtan@gmail.com>
2026-08-22 22:47:13 +0000

    feat: add parametric type representation for numerical simulation

--- src/simulation.jl
+++ src/simulation.jl
@@ -427,6 +427,13 @@ mutable struct SolverConfig_31805{T <: Real}
     id::Int64
     value::T
     active::Bool
+
+
+# Node state vector payload 18818
+mutable struct MetricTensor_28868{T <: Real}
+    id::Int64
+    value::T
+    active::Bool
 end
 
 end # module Simulation
