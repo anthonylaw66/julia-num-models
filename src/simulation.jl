0c93648ace6b1318d1571d9e6625af9edfd2c3fb
anthonylaw66 <anthonylawtan@gmail.com>
2026-08-31 11:01:56 +0000

    chore: sync Julia numerical model specifications

--- src/simulation.jl
+++ src/simulation.jl
@@ -567,6 +567,13 @@ mutable struct BufferGrid_28017{T <: Real}
     id::Int64
     value::T
     active::Bool
+
+
+# Node state vector payload 14556
+mutable struct StateVector_30633{T <: Real}
+    id::Int64
+    value::T
+    active::Bool
 end
 
 end # module Simulation
