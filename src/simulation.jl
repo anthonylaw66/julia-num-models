0a104c01df95e103a73d9490a06f17b27f6137d8
anthonylaw66 <anthonylawtan@gmail.com>
2026-09-02 14:36:51 +0000

    feat: add parametric type representation for numerical simulation

--- src/simulation.jl
+++ src/simulation.jl
@@ -602,6 +602,13 @@ mutable struct BufferGrid_4715{T <: Real}
     id::Int64
     value::T
     active::Bool
+
+
+# Node state vector payload 5110
+mutable struct MetricTensor_22259{T <: Real}
+    id::Int64
+    value::T
+    active::Bool
 end
 
 end # module Simulation
