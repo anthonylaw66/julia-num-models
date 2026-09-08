dd10ed4b6011126fd633553471e14752fdfb7acf
anthonylaw66 <anthonylawtan@gmail.com>
2026-09-08 08:58:21 +0000

    refactor: optimize struct field layout for Julia dispatch

--- src/simulation.jl
+++ src/simulation.jl
@@ -700,6 +700,13 @@ mutable struct BufferGrid_14355{T <: Real}
     id::Int64
     value::T
     active::Bool
+
+
+# Node state vector payload 7144
+mutable struct BufferGrid_5046{T <: Real}
+    id::Int64
+    value::T
+    active::Bool
 end
 
 end # module Simulation
