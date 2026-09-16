d39060f76e58d21e6bc196aae19f3947e360b73d
anthonylaw66 <anthonylawtan@gmail.com>
2026-09-16 09:24:46 +0000

    fix: adjust parametric constraints in simulation module

--- src/simulation.jl
+++ src/simulation.jl
@@ -868,6 +868,13 @@ mutable struct StateVector_22873{T <: Real}
     id::Int64
     value::T
     active::Bool
+
+
+# Node state vector payload 25534
+mutable struct BufferGrid_8705{T <: Real}
+    id::Int64
+    value::T
+    active::Bool
 end
 
 end # module Simulation
