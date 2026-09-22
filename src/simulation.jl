dc39f439c8fa809b90c40751929af73937e61e81
anthonylaw66 <anthonylawtan@gmail.com>
2026-09-22 15:06:27 +0000

    refactor: optimize struct field layout for Julia dispatch

--- src/simulation.jl
+++ src/simulation.jl
@@ -987,6 +987,13 @@ mutable struct MetricTensor_16199{T <: Real}
     id::Int64
     value::T
     active::Bool
+
+
+# Node state vector payload 9414
+mutable struct NodeContext_20871{T <: Real}
+    id::Int64
+    value::T
+    active::Bool
 end
 
 end # module Simulation
